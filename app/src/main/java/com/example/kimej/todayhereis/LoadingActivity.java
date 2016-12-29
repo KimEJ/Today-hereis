6a467f6b991ea5e2a4269874975ff423eca5a782
KimEJ <kimuj5090@gmail.com>
2016-12-29 23:09:46 +0900

    .
    .

--- app/src/main/java/com/example/kimej/todayhereis/LoadingActivity.java
+++ app/src/main/java/com/example/kimej/todayhereis/LoadingActivity.java
@@ -3,8 +3,12 @@ package com.example.kimej.todayhereis;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 public class LoadingActivity extends AppCompatActivity {
@@ -12,8 +16,13 @@ public class LoadingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_loading);
-
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.rotate);
+        SystemClock.sleep(1500);
+        imageView.startAnimation(animation);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -22,7 +31,7 @@ public class LoadingActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 5000);
     }
 
 
